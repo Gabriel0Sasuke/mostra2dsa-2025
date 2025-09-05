af8fbd637cc6734de83169160c655dc110521952
deservs <carlos.rodrigues151@etec.sp.gov.br>
2025-09-05 08:10:19 -0300

    pagina do hitchcock

--- PHP/sobre/hitchcock.php
+++ PHP/sobre/hitchcock.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sobre Hitchcock</title>
+    <link rel="stylesheet" href="../../css/homi.css">
 </head>
+
 <body>
-    
+    <h1>Sobre Hitchcock</h1>
+    <div id="sobre">
+        <div id="lado">
+            <img id="cara" src="../../images/Hitchcock.png" alt="">
+            <div id="texto">
+                <div id="bio">Sua biografia</div>
+                <div id="txt">Alfred Hitchcock, conhecido como "Mestre do Suspense", nasceu em Londres em 13 de agosto de 1899. Ele começou sua carreira no cinema em 1919, inicialmente como designer de intertítulos.</div>
+            </div>
+        </div>
+        <div id="txt2">Seu primeiro filme como diretor foi "The Pleasure Garden" em 1925. Hitchcock é famoso por seus thrillers da década de 30, como The 39 Steps" e "The Lady Vanishes". Em 1940, ele se mudou para Hollywood e dirigiu "Rebecca", que foi indicado em onze categorias do Oscar e venceu o prêmio de Melhor Filme. Hitchcock também é conhecido por sua apresentação do programa "Alfred Hitchcock Presents" e por suas frequentes aparições em seus filmes.</div>
+    </div>
+    <img id="birdu" src="../../images/birdu.png" alt="">
+    </div>
 </body>
+
 </html>
